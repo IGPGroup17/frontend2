404a78a823bc0253a14fe9e0a9a3b64968c4d9be
jlacey7474 <jlacey7474@gmail.com>
2021-03-22 20:45:45 +0000

    added some models - probably have redundant information in them

--- app/src/main/java/com/example/personalprofile/models/Event.java
+++ app/src/main/java/com/example/personalprofile/models/Event.java
@@ -2,9 +2,17 @@ package com.example.personalprofile.models;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Event {
 
     private String name;
--- app/src/main/java/com/example/personalprofile/models/Student.java
+++ app/src/main/java/com/example/personalprofile/models/Student.java
@@ -1,8 +1,16 @@
 package com.example.personalprofile.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
 
     private String username;
