9602913d444a0ead6a2d20f5eeb95e9ba52e94f0
OllieJonas <olliejonas@gmail.com>
2021-03-24 20:09:28 +0000

    Started CreateEventActivity

--- app/src/main/java/com/example/personalprofile/activities/CreateEventActivity.java
+++ app/src/main/java/com/example/personalprofile/activities/CreateEventActivity.java
@@ -4,10 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.example.personalprofile.R;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class CreateEventActivity extends AppCompatActivity {
+
+    private TextView name;
+
+    private TextView description;
+
+    private TextView date;
+
+    private TextView time;
+
+    private AtomicBoolean isAlcoholFree;
+
+    private AtomicBoolean isVirtual;
+
+    private AtomicBoolean isInPerson;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
